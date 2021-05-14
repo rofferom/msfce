d35283191dc543d174597c207fb60f06d3cbe31b
Romain Roffé <rofferom@gmail.com>
2021-05-14 13:38:14 +0200

    wram: Implement indirect access through registers

---
+++ src/wram.cpp
@@ -0,0 +1,63 @@
+#include "log.h"
+#include "registers.h"
+#include "wram.h"
+
+#define TAG "wram"
+
+Wram::Wram() : BufferMemComponent(
+    MemComponentType::ram,
+    kWramSize)
+{
+}
+
+IndirectWram::IndirectWram(const std::shared_ptr<Wram>& wram)
+    : MemComponent(MemComponentType::indirectRam)
+{
+}
+
+uint8_t IndirectWram::readU8(uint32_t addr)
+{
+    auto ret = m_Wram->readU8(m_Address);
+
+    switch (addr) {
+    case kRegisterWMDATA: {
+        auto value = m_Wram->readU8(m_Address);
+        m_Address++;
+        return value;
+    }
+
+    default:
+        LOGW(TAG, "Ignore ReadU8 at %06X", addr);
+        return 0;
+    }
+}
+
+void IndirectWram::writeU8(uint32_t addr, uint8_t value)
+{
+    switch (addr) {
+    case kRegisterWMDATA: {
+        m_Wram->writeU8(m_Address, value);
+        m_Address++;
+        break;
+    }
+
+    case kRegisterWMADDL:
+        m_Address &= 0xFFFF00;
+        m_Address |= value;
+        break;
+ 
+    case kRegisterWMADDM:
+        m_Address &= 0xFF00FF;
+        m_Address |= value << 8;
+        break;
+
+    case kRegisterWMADDH:
+        m_Address &= 0xFFFF;
+        m_Address |= value << 16;
+        break;
+
+    default:
+        LOGW(TAG, "Ignore WriteU8 %02X at %06X", value, addr);
+        break;
+    }
+}
---
+++ src/wram.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include "memcomponent.h"
+
+class Wram : public BufferMemComponent {
+public:
+    Wram();
+};
+
+class IndirectWram : public MemComponent {
+public:
+    IndirectWram(const std::shared_ptr<Wram>& wram);
+
+    uint8_t readU8(uint32_t address) override;
+    void writeU8(uint32_t address, uint8_t value) override;
+
+private:
+    std::shared_ptr<Wram> m_Wram;
+
+    uint32_t m_Address = 0;
+};
