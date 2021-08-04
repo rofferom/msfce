27c243e91c088f3de7d44750dab4fea5a651df1e
Romain Roffé <rofferom@gmail.com>
2021-08-04 11:37:36 +0200

    controller: Implement manual reading

--- src/controller.cpp
+++ src/controller.cpp
@@ -54,10 +54,14 @@ uint8_t ControllerPorts::readU8(uint32_t addr)
     case kRegisterJoy1H: {
         return m_Joypad1Register >> 8;
     }
+    case kRegisterJoyA: {
+        uint8_t value = (m_Controller1_ReadReg >> 15) & 1;
+        m_Controller1_ReadReg = (m_Controller1_ReadReg << 1) | 1;
+        return value;
+    }
 
     case kRegisterJoy2L:
     case kRegisterJoy2H:
-    case kRegisterJoyA:
     case kRegisterJoyB:
     case kRegisterJoy3L:
     case kRegisterJoy3H:
@@ -76,6 +80,17 @@ void ControllerPorts::writeU8(uint32_t addr, uint8_t value)
 {
     switch (addr) {
     case kRegisterJoyWr:
+        // Avoid to fill read register if sequence is incorrect
+        if (m_Controller1_Strobe == value) {
+            return;
+        }
+
+        m_Controller1_Strobe = value;
+
+        // Read controller
+        m_Controller1_ReadReg = packController(m_Controller1_State);
+        break;
+
     case kRegisterJoyWrio:
         break;
 
@@ -89,30 +104,41 @@ void ControllerPorts::writeU8(uint32_t addr, uint8_t value)
 
 void ControllerPorts::setController1(const SnesController& controller)
 {
-    m_Controller1 = controller;
+    m_Controller1_State = controller;
 }
 
 void ControllerPorts::readController()
 {
-    m_Joypad1Register = 0;
+    m_Joypad1Register = packController(m_Controller1_State);
+}
+
+uint16_t ControllerPorts::packController(const SnesController& controller)
+{
+    uint16_t value = 0; // Already contains the standard controler ID (0b0000)
 
     for (size_t i = 0; i < SIZEOF_ARRAY(s_ControllerRegisterMap); i++) {
         const auto& mapping = s_ControllerRegisterMap[i];
 
-        if (controllerGetButton(&m_Controller1, mapping)) {
-            m_Joypad1Register |= (1 << mapping.bit);
+        if (controllerGetButton(&controller, mapping)) {
+            value |= (1 << mapping.bit);
         }
     }
+
+    return value;
 }
 
 void ControllerPorts::dumpToFile(FILE* f)
 {
-    fwrite(&m_Controller1, sizeof(m_Controller1), 1, f);
+    fwrite(&m_Controller1_State, sizeof(m_Controller1_State), 1, f);
     fwrite(&m_Joypad1Register, sizeof(m_Joypad1Register), 1, f);
+    fwrite(&m_Controller1_Strobe, sizeof(m_Controller1_Strobe), 1, f);
+    fwrite(&m_Controller1_ReadReg, sizeof(m_Controller1_ReadReg), 1, f);
 }
 
 void ControllerPorts::loadFromFile(FILE* f)
 {
-    fread(&m_Controller1, sizeof(m_Controller1), 1, f);
+    fread(&m_Controller1_State, sizeof(m_Controller1_State), 1, f);
     fread(&m_Joypad1Register, sizeof(m_Joypad1Register), 1, f);
+    fread(&m_Controller1_Strobe, sizeof(m_Controller1_Strobe), 1, f);
+    fread(&m_Controller1_ReadReg, sizeof(m_Controller1_ReadReg), 1, f);
 }
