76393704ed9e5646ca44d887f547f309cfe456bb
Romain Roffé <rofferom@gmail.com>
2021-08-26 14:57:19 +0200

    app: Improve command line parsing (and add --help)

--- app/main.cpp
+++ app/main.cpp
@@ -1,27 +1,83 @@
+#include <stdio.h>
+#include <getopt.h>
+
 #include <msfce/core/log.h>
 #include <msfce/core/snes.h>
 #include "frontend_sdl2/frontend_sdl2.h"
 
 #define TAG "main"
 
-int main(int argc, char* argv[])
+struct Params {
+    bool help;
+    bool verbose;
+};
+
+int parseArgs(int argc, char* argv[], Params* params)
 {
+    int optionIndex = 0;
+    int value;
     int ret;
 
-    if (argc < 2) {
-        LOGE(TAG, "Missing ROM arg");
-        return 1;
+    const struct option argsOptions[] = {
+        {"help", optional_argument, 0, 'h'},
+        {"verbose", optional_argument, 0, 'v'},
+        {0, 0, 0, 0}};
+
+    while (true) {
+        value = getopt_long(argc, argv, "hv", argsOptions, &optionIndex);
+        if (value == -1 || value == '?')
+            break;
+
+        switch (value) {
+        case 'h':
+            params->help = true;
+            break;
+
+        case 'v':
+            params->verbose = true;
+            break;
+
+        default:
+            break;
+        }
     }
 
-    // logSetLevel(LOG_DEBUG);
+    return 0;
+}
 
-    LOGI(TAG, "Welcome to Monkey Super Famicom Emulator");
+void printHelp(int argc, char* argv[])
+{
+    printf("Usage: %s [-h] [-v] rom\n\n", argv[0]);
+
+    printf("positional arguments:\n");
+    printf("  %-20s %s\n", "rom", "Rom to load");
+    printf("\n");
+
+    printf("optional arguments:\n");
+    printf("  %-20s %s\n", "-h, --help", "show this help message and exit");
+    printf("  %-20s %s\n", "-v, --verbose", "add extra logs");
+}
+
+int main(int argc, char* argv[])
+{
+    Params params;
+    int ret;
+
+    ret = parseArgs(argc, argv, &params);
+    if (ret < 0 || optind == argc) {
+        printHelp(argc, argv);
+        return 0;
+    }
+
+    if (params.verbose) {
+        logSetLevel(LOG_DEBUG);
+    }
 
     // Create frontend
     auto frontend = std::make_shared<FrontendSdl2>();
 
     // Create and run SNES
-    const char* romPath = argv[1];
+    const char* romPath = argv[optind];
 
     auto snes = msfce::core::Snes::create();
     snes->addRenderer(frontend);
